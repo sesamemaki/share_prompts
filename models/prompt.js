a2c8a072edbb523163fe68491bf6c83be3cb945e
Burcu Bayraö <burju7@yahoo.com>
2023-06-19 22:52:16 +0300

    completed create page

---
+++ models/prompt.js
@@ -0,0 +1,19 @@
+import { Schema, model, models } from "mongoose";
+import mongoose from "mongoose";
+
+
+const PromptSchema = new Schema({
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  prompt: {
+    type: String,
+    required: [true, "Prompt is required"],
+  },
+  tag: { type: String, required: [true, "Prompt is required"] },
+});
+
+const Prompt = models.Prompt || model("Prompt", PromptSchema);
+
+export default Prompt;
