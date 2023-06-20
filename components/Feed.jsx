bcaa054026a23085c68e10a7afe8cfc848b9dbb9
Burcu Bayraö <burju7@yahoo.com>
2023-06-20 12:03:11 +0300

    last version

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -6,7 +6,11 @@ const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className="mt-16 prompt_layout">
       {data.map((post) => (
-        <PromptCard key={post.id} post={post} handleTagClick={handleTagClick} />
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
       ))}
     </div>
   );
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,6 +5,10 @@ import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
   const [copied, setCopied] = useState("");
 
   const handleCopy = () => {
@@ -52,8 +56,20 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
+      {session?.user.id === post.creator?._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green-gradient cursor-pointer"
+            onClick={handleEdit}
+          ></p>
+          <p
+            className="font-inter text-sm orange-gradient cursor-pointer"
+            onClick={handleDelete}
+          ></p>
+        </div>
+      )}
     </div>
   );
 };
